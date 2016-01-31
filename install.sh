85ddfb17f95d28a14343f50f5e163f7e5ff7122a
binhex <megalith01@gmail.com>
2016-01-31 22:33:49 +0000

    install file complete

--- install.sh
+++ install.sh
@@ -4,25 +4,15 @@
 set -e
 
 # define pacman packages
-pacman_packages="unzip unrar librsvg pygtk python2-service-identity python2-mako python2-notify deluge"
+pacman_packages="emby"
 
 # install pre-reqs
 pacman -Syu --ignore filesystem --noconfirm
 pacman -S --needed $pacman_packages --noconfirm
 
 # set permissions
-chown -R nobody:users /usr/bin/deluged /usr/bin/deluge-web
-chmod -R 775 /usr/bin/deluged /usr/bin/deluge-web
-
-# add in temporary fixes for torrents stuck in paused mode
-curl -o /tmp/libtorrent-rasterbar.tar.xz -L http://ala.seblu.net/packages/l/libtorrent-rasterbar/libtorrent-rasterbar-1%3A1.0.7-2-x86_64.pkg.tar.xz
-pacman -U /tmp/libtorrent-rasterbar.tar.xz --noconfirm
-
-curl -o /tmp/boost-libs.tar.xz -L http://ala.seblu.net/packages/b/boost-libs/boost-libs-1.59.0-5-x86_64.pkg.tar.xz
-pacman -U /tmp/boost-libs.tar.xz --noconfirm
-
-curl -o /tmp/boost.tar.xz -L http://ala.seblu.net/packages/b/boost/boost-1.59.0-5-x86_64.pkg.tar.xz
-pacman -U /tmp/boost.tar.xz --noconfirm
+chown -R nobody:users /etc/conf.d/emby-server /usr/lib/emby-server /usr/bin/emby-server
+chmod -R 775 /etc/conf.d/emby-server /usr/lib/emby-server /usr/bin/emby-server
 
 # cleanup
 yes|pacman -Scc
