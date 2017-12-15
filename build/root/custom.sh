707861c60caa9b3bbc811e8b3df5569e4455c7bd
binhex <megalith01@gmail.com>
2017-12-15 22:40:22 +0000

    drop ext for pkg

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -5,7 +5,7 @@ set -e
 
 # install dotnet-host (emby reports bug wiuth latest dotnet)
 pkg_name="dotnet-host"
-pkg_ver="2.0.0-2-x86_64.pkg.tar"
+pkg_ver="2.0.0-2-x86_64"
 
 # download compiled package(s) from binhex repo
 /root/curly.sh -rc 6 -rw 10 -of "/tmp/${pkg_name}-${pkg_ver}.pkg.tar.xz" -url "https://github.com/binhex/arch-packages/raw/master/compiled/${pkg_name}-${pkg_ver}.pkg.tar.xz"
@@ -13,7 +13,7 @@ pacman -U "/tmp/${pkg_name}-${pkg_ver}.pkg.tar.xz" --noconfirm
 
 # install dotnet-runtime (emby reports bug wiuth latest dotnet)
 pkg_name="dotnet-runtime"
-pkg_ver="2.0.0-2-x86_64.pkg.tar"
+pkg_ver="2.0.0-2-x86_64"
 
 # download compiled package(s) from binhex repo
 /root/curly.sh -rc 6 -rw 10 -of "/tmp/${pkg_name}-${pkg_ver}.pkg.tar.xz" -url "https://github.com/binhex/arch-packages/raw/master/compiled/${pkg_name}-${pkg_ver}.pkg.tar.xz"
