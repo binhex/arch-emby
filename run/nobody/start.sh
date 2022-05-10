3a37b5ea0e7c836c7d73bea3ed4471c68637b4c2
binhex <megalith01@gmail.com>
2022-05-10 12:36:40 +0100

    more tweaks to emby

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -3,10 +3,11 @@
 # config below is taken from bash script /usr/bin/emby-server.sh
 
 # set env variables for emby
-export MONO='/usr/bin/mono'
 export PROGRAM_DATA='/config'
 export FFMPEG='/usr/bin/ffmpeg'
 export FFPROBE='/usr/bin/ffprobe'
+export FFDETECT='/usr/bin/ffdetect-emby'
+export LIBVA_DRIVERS_PATH='/usr/lib/dri'
 
 # run emby-server
 exec '/usr/bin/emby-server'
